a9a3ada6654cd8be0d210927d83cfb39c701ba42
monicams88 <monicamelati24@gmail.com>
2020-04-28 20:14:51 +0800

    Switch

---
+++ src/Percabangan/Switch.java
@@ -0,0 +1,26 @@
+package Percabangan;
+
+public class Switch {
+    public static void main(String [] args){
+        int jalanJalan=2;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang malmingan");
+                break;
+            case 3:
+                System.out.println("obat nyamuk nah");
+                break;
+            case 4:
+                System.out.println("pedekate");
+                break;
+            case 5:
+                System.out.println("ghaib");
+                break;
+
+
+        }
+    }
+}
